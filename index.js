f828b945f3672817bfd129166f7c4f8da0776c3c
jitbherwaniofficial <jitbherwani92@gmail.com>
2024-07-28 21:19:13 +0530

    Commit Bas

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const nav = document.getElementById("nav");
 
 window.onscroll = function () {
     let top = window.scrollY;
-    if (top > 200) {
+    if (top > 100) {
       nav.classList.add("active");
     } else {
       nav.classList.remove("active");
     }
-  };
+  }; 
