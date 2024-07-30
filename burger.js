dda7607b21324e329526ff78dbd21d980d9fd139
jitbherwaniofficial <jitbherwani92@gmail.com>
2024-07-31 02:14:21 +0530

    thodu si update hai

--- burger.js
+++ burger.js
@@ -1,6 +1,11 @@
 // MENU //
-const burger = document.querySelector(".burger");
-const link = document.querySelector(".link");
+const burger = document.getElementById("burger");
+const link2 = document.querySelector(".link2");
 burger.addEventListener("click", () => {
-    link.classList.toggle("active");
+  link2.classList.toggle("active");
+  if (link2.classList.contains("active")) {
+    burger.classList.add("active");
+  } else {
+    burger.classList.remove("active");
+  }
 });
--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-
 const nav = document.getElementById("nav");
 
 window.onscroll = function () {
-    let top = window.scrollY;
-    if (top > 100) {
-      nav.classList.add("active");
-    } else {
-      nav.classList.remove("active");
-    }
-  }; 
+  let top = window.scrollY;
+  if (top > 100) {
+    nav.classList.add("active");
+  } else {
+    nav.classList.remove("active");
+  }
+};
