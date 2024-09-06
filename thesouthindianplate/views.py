971cec69c50af1629da954da36fdd1e2f7c603e1
jitbherwaniofficial <jitbherwani92@gmail.com>
2024-09-06 16:17:13 +0530

    404 part 5

--- thesouthindianplate/views.py
+++ thesouthindianplate/views.py
@@ -5,5 +5,6 @@ from django.shortcuts import render
 def robot(request):
     return render(request, 'robots.txt')
 
-def custom_page_not_found(request, *args, template_name="404.html", **kargs):
-    return render(None, template_name, status=404)
+def error_404(request, exception):
+    data = {"name": "thesouthindianplate.com"}
+    return render(request,'404.html', data)
