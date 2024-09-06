cc089ead6fb767c7217cde0d94164a47521aa7e4
jitbherwaniofficial <jitbherwani92@gmail.com>
2024-09-06 15:18:43 +0530

    Changed Debug To False

--- thesouthindianplate/settings.py
+++ thesouthindianplate/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q$-m=i#)q7r4grb)lvwls9ac@0^zhz8(9t4+b=xiv^x9)7nfg='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app','127.0.0.1', 'localhost','.now.sh', 'thesouthindianplate.com']
 
