56126f6a29cc93131dffa9036adc85b5a0755085
jitbherwaniofficial <jitbherwani92@gmail.com>
2024-09-07 17:18:00 +0530

    DateTime Updated In Sitemap

--- thesouthindianplate/views.py
+++ thesouthindianplate/views.py
@@ -15,50 +15,50 @@ def sitemap_view(request):
     urls = [
         {
             "loc": "https://thesouthindianplate.com/",
-            "lastmod": timezone.now().date(),
+            "lastmod": '2024-09-07T11:09:00+00:00',
             "priority": "1.0"
         },
         {
             "loc": "https://thesouthindianplate.com/how-to-order/",
-            "lastmod": timezone.now().date(),
+            "lastmod": '2024-09-07T11:09:00+00:00',
             "priority": "0.8"
         },
         {
             "loc": "https://thesouthindianplate.com/best-idli-order-mumbai/",
-            "lastmod": timezone.now().date(),
+            "lastmod": '2024-09-07T11:09:00+00:00',
             "priority": "0.8"
         },
         {
             "loc": "https://thesouthindianplate.com/best-meduvada-order-mumbai/",
-            "lastmod": timezone.now().date(),
+            "lastmod": '2024-09-07T11:09:00+00:00',
             "priority": "0.8"
         },
         {
             "loc": "https://thesouthindianplate.com/best-dalvada-order-mumbai/",
-            "lastmod": timezone.now().date(),
+            "lastmod": '2024-09-07T11:09:00+00:00',
             "priority": "0.8"
         },
         {
             "loc": "https://thesouthindianplate.com/menu-page/",
-            "lastmod": timezone.now().date(),
+            "lastmod": '2024-09-07T11:09:00+00:00',
             "priority": "0.8"
         },
         {
             "loc": "https://thesouthindianplate.com/contact-us/",
-            "lastmod": timezone.now().date(),
+            "lastmod": '2024-09-07T11:09:00+00:00',
             "priority": "0.8"
         },
         {
             "loc": "https://thesouthindianplate.com/privacy-policy/",
-            "lastmod": timezone.now().date(),
+            "lastmod": '2024-09-07T11:09:00+00:00',
             "priority": "0.8"
         },
         {
             "loc": "https://thesouthindianplate.com/terms-and-conditions/",
-            "lastmod": timezone.now().date(),
+            "lastmod": '2024-09-07T11:09:00+00:00',
             "priority": "0.8"
         },
-        # Add more URLs dynamically or manually here
+        
     ]
     sitemap_xml = render_to_string("sitemap.xml", {"urls": urls})
     return HttpResponse(sitemap_xml, content_type="application/xml")
