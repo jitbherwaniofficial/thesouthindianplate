8568e499671bdff1be1e176074e020b2044e8ab0
jitbherwaniofficial <jitbherwani92@gmail.com>
2024-09-07 14:38:33 +0530

    Localhost deleted

--- thesouthindianplate/settings.py
+++ thesouthindianplate/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q$-m=i#)q7r4grb)lvwls9ac@0^zhz8(9t4+b=xiv^x9)7nfg=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app','.now.sh', 'thesouthindianplate.com','www.thesouthindianplate.com','127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app','.now.sh', 'thesouthindianplate.com','www.thesouthindianplate.com']
 
 
 # Application definition
