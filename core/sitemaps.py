162b96862fc5dea39ef6074bc518a678a8d193ad
jitbherwaniofficial <jitbherwani92@gmail.com>
2024-09-11 00:33:54 +0530

    Robot.TXT UPDATED3

--- core/sitemaps.py
+++ core/sitemaps.py
@@ -2,8 +2,8 @@ from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
 
 class StaticViewSitemap(Sitemap):
-    changefreq = 'daily'
-
+    
+    # This method defines the items (URLs) to include in the sitemap.
     def items(self):
         return [
             'home',
@@ -17,6 +17,7 @@ class StaticViewSitemap(Sitemap):
             'terms_and_conditions'
         ]
 
+    # This method sets the priority for specific URLs.
     def priority(self, item):
         if item == 'home':
             return 1.0  # Highest priority for home page
@@ -25,5 +26,13 @@ class StaticViewSitemap(Sitemap):
         else:
             return 0.9  # Default priority for other pages
 
+    # This method sets the change frequency for specific URLs.
+    def changefreq(self, item):
+        if item in ['privacy_policy', 'terms_and_conditions']:
+            return 'monthly'  # Set privacy policy and terms to monthly
+        else:
+            return 'weekly'  # Set all other pages to weekly
+
+    # This method returns the URL location of each item.
     def location(self, item):
         return reverse(item)
