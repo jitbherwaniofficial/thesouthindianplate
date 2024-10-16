aab46465d5797e991740183332aa0d9f9544b8ee
jitbherwaniofficial <jitbherwani92@gmail.com>
2024-10-16 16:08:23 +0530

    New Update

--- core/urls.py
+++ core/urls.py
@@ -6,12 +6,6 @@ from core import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('how-to-order', views.howToOrder, name='how-to-order'),
-    path('menu-page', views.menu, name='menu'),
-    path('best-dalvada-order-mumbai', views.dalvada, name='dalvada'),
-    path('best-idli-order-mumbai', views.idli, name='idli'),
-    path('best-meduvada-order-mumbai', views.meduvada, name='meduvada'),
-    path('contact-us', views.contact, name='contact'),
     path('privacy-policy', views.privacy_policy, name='privacy_policy'),
     path('terms-and-conditions', views.terms_and_conditions, name='terms_and_conditions'),
     path('robots.txt', views.robots_txt, name='robots_txt'),
--- core/views.py
+++ core/views.py
@@ -6,24 +6,6 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'index.html')
 
-def howToOrder(request):
-    return render(request, 'how-to-order.html')
-
-def menu(request):
-    return render(request, 'menu.html')
-
-def dalvada(request):
-    return render(request, 'dalvada.html')
-
-def idli(request):
-    return render(request, 'idli.html')
-
-def meduvada(request):
-    return render(request, 'meduvada.html')
-
-def contact(request):
-    return render(request, 'contact-us.html')
-
 
 def privacy_policy(request):
     return render(request, 'privacy-policy.html')
@@ -31,7 +13,6 @@ def privacy_policy(request):
 
 def terms_and_conditions(request):
     return render(request, 'terms-and-conditions.html')
-
 
 
 from django.template import loader
